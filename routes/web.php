51c4051185cbb2e2f398614b8cccaac4cd68e55a
karansin123 <51689241+karansin123@users.noreply.github.com>
2019-09-17 23:05:03 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('/', 'users', function () {
     return 'Users 543';
 });
