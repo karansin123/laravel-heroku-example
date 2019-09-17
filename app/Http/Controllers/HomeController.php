6cc4a3abf8b9c757a73e46ec1d93d75d2d1a637d
karansin123 <51689241+karansin123@users.noreply.github.com>
2019-09-18 00:05:12 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,8 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
-{ 
-     function showHello(){
-      return '<h1>New Controller of : Karansin Khotmungkun</h1>';
-     }
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
 }
