6e1cd457f4442b31a61db5ea64679d3411366f37
karansin123 <51689241+karansin123@users.noreply.github.com>
2019-09-18 00:01:31 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,24 +10,3 @@ class HomeController extends Controller
       return '<h1>New Controller of : Karansin Khotmungkun</h1>';
      }
 }
-{
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view('home');
-    }
-}
