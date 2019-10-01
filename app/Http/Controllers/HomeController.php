0f864e048bd07f1263f8e567ac3a7762b28a3edf
karansin123 <51689241+karansin123@users.noreply.github.com>
2019-10-01 10:10:49 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,27 +6,7 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view('home');
-    }
-}
-{
-    function showhello($name){
-        return '<h1>Hello:'.$name.'</h1>';
-    }
+function showhello($name){
+   return '<h1>Hello:'.$name.'</h1>';
+   }
 }
