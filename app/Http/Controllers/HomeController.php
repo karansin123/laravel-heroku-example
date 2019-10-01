20a2a24df5c4a08997be3a6c7aa5b0d51f9bea5f
karansin123 <51689241+karansin123@users.noreply.github.com>
2019-10-01 13:04:27 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class HelloController extends Controller
 {
      function show(){
        return view('users')
