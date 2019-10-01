5566320faaa0fa309ae4c86ce25ae74ad486447b
karansin123 <51689241+karansin123@users.noreply.github.com>
2019-10-01 12:53:55 +0700

    Update Hellocontrollers.php

--- app/Http/Controllers/Hellocontrollers.php
+++ app/Http/Controllers/Hellocontrollers.php
@@ -1,3 +1,15 @@
 <?php
 
-namespace App
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    function showHello($name){
+       return '<h1>Hello:'.$name.'</h1>';
+    }
+    function resultProduct($product){
+       return '<h1>Show:'.$product.'</h1>';
+    }
+}
