b04bafe6cd7800d82f77be54a807aa63ab591bd3
karansin123 <51689241+karansin123@users.noreply.github.com>
2019-10-01 15:02:37 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravelsa
                 </div>
 
                 <div class="links">
