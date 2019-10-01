b6169e20a640613be46963276e78e23edfe8bd70
karansin123 <51689241+karansin123@users.noreply.github.com>
2019-10-01 12:18:32 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-function showHello(name){
-   return '<h1>Hello:'.name.'</h1>';
+function showHello($name){
+   return '<h1>Hello:'.$name.'</h1>';
    }
 }
